ebb263ff106e9050e891b08d5e32d2dbe0673de7
ChristofferMikkelsen <mikkelsen3@gmail.com>
2015-09-22 10:28:04 +0200

    test 2 og 3

--- GitHubDemo/src/githubdemo/GitHubDemo.java
+++ GitHubDemo/src/githubdemo/GitHubDemo.java
@@ -8,6 +8,7 @@ public class GitHubDemo {
     public static void main(String[] args) {
         System.out.println("Test 1");
         System.out.println("test 2");
+        System.out.println("test 3");
     }
     
 }
